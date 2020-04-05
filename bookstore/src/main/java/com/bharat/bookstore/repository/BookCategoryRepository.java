6213d2c72985dcf08f5674862506fcea0aafb253
bharatvarmagit <itisbharatvarma@gmail.com>
2020-04-05 15:33:52 -0400

    completed backend for feature One.

--- bookstore/src/main/java/com/bharat/bookstore/repository/BookCategoryRepository.java
+++ bookstore/src/main/java/com/bharat/bookstore/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package com.bharat.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bharat.bookstore.model.BookCategory;
 
+@RepositoryRestResource(collectionResourceRel = "BookCategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }

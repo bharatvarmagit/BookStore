fb228d942f90a19208e65ee226784ed0da30efe5
bharatvarmagit <itisbharatvarma@gmail.com>
2020-04-07 12:40:26 -0400

    updated from table to grid system using bootstrap 4

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookService } from './services/book.service';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     HttpClientModule,

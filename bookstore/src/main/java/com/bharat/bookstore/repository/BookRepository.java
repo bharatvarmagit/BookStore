d403bf874e8bf30fb8d4f0ea0aa26b13b8b0d97c
bharatvarmagit <itisbharatvarma@gmail.com>
2020-04-12 12:30:36 -0400

    added search_by_keyword query method i Book repositor

--- bookstore/src/main/java/com/bharat/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/com/bharat/bookstore/repository/BookRepository.java
@@ -11,5 +11,6 @@ import com.bharat.bookstore.model.Book;
 public interface BookRepository extends JpaRepository<Book, Long>{
 	@RestResource(path = "categoryid")
 	Page<Book> findByCategoryId(@Param("id") Long id,Pageable pageable);
-
+	@RestResource(path="searchbykeyword")
+	Page<Book> findByNameContaining(@Param("name") String keyword,Pageable pageable);
 }
